5d99a405c4f92be815c41dbadc14b3597c628e31
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2024-11-29 19:40:51 +0530

    ALONE CODER

--- AloneMusic/plugins/sudo/logger.py
+++ AloneMusic/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AviaxMusic import app
-from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.database import add_off, add_on
-from AviaxMusic.utils.decorators.language import language
+from AloneMusic import app
+from AloneMusic.misc import SUDOERS
+from AloneMusic.utils.database import add_off, add_on
+from AloneMusic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
