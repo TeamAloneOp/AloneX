6fb028388be8ce7061dd3846e38d21a74d0e4f6c
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-02-17 08:00:35 +0530

    𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ✨

--- AloneX/plugins/bot/music.py
+++ AloneX/plugins/bot/music.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AloneX import app
 from config import BOT_USERNAME
-from AloneX.utils.errors import capture_err
+#from AloneX.utils.errors import capture_err
 import httpx 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
