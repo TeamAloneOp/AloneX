2603103e6eb8ae8a58f351867c6e340c6ec9adf4
maikyobataubsdk-ux <maikyobataubsdk@gmail.com>
2026-06-10 14:16:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/vertexxtg")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/vertex_Era")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
